a84f82b2f3ff5139597e198ec8c23d9ec15e2389
gorbenco03 <chiril.gorbenco@student.upt.ro>
2024-08-10 16:31:05 -0400

    fix issues

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -33,7 +33,7 @@ const ProductList: React.FC<ProductListProps> = ({ products, addToCart }) => {
         {products.map((product) => (
           <div key={product._id} className="bg-white shadow-md rounded-lg p-4" onClick={() => openModal(product)}>
             <img
-              src={`https://ecomplazza.serveftp.com/${product.imageUrl}`}
+              src={`https://ecomplazza.serveftp.com${product.imageUrl}`}
               alt={product.name}
               className="w-full h-48 object-cover rounded-lg mb-4"
             />
