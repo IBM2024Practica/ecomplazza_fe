88bef7d60d8e9d3b03b79ae31296e6318072ac94
gorbenco03 <chiril.gorbenco@student.upt.ro>
2024-08-11 13:16:04 -0400

    hz

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -17,7 +17,7 @@ const FavouritesPage: React.FC = () => {
           {favouriteItems.map((product) => (
             <div key={product._id} className="bg-white shadow-md rounded-lg p-4">
               <img
-                src={`https://ecomplazza.serveftp.com/${product.imageUrl}`}
+                src={`https://ecomplazza.serveftp.com${product.imageUrl}`}
                 alt={product.name}
                 className="w-full h-48 object-cover rounded-lg mb-4"
               />
